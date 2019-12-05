7d4a0f7ef72b7b1b623201936363b39ff9809d05
CorentinTh <corentin.thomasset74@gmail.com>
2019-12-05 16:27:51 +0100

    feat: added cli

---
+++ bin/cli.js
@@ -0,0 +1,20 @@
+const {bame} = require('..');
+
+var argv = require('yargs')
+    .usage('Usage: bame <inReg> <outReg>')
+    .option('d', {alias: 'no-dir', boolean:true, description:'Should not rename directories'})
+    .option('q', {alias: 'quiet', boolean:true, description:'Rename silently'})
+    .demandCommand(2)
+    .alias('h', 'help')
+    .alias('v', 'version')
+    .example('bame "(.*) Episode(.*)" "{1}.E{2}"')
+    .argv;
+    
+bame({
+    inReg: argv._[0],
+    outReg: argv._[1],
+    options:{
+        renameDir: !argv.d,
+        verbose: !argv.q
+    }
+})
